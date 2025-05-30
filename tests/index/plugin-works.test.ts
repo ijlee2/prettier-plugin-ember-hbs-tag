3da882d1b2454434e9b7460fff9a4436df9ac3fb
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-05-30 06:37:01 +0200

    chore: Tested plugin

---
+++ tests/index/plugin-works.test.ts
@@ -0,0 +1,47 @@
+import { assert, test } from '@codemod-utils/tests';
+
+import { formatFile } from '../helpers/index.js';
+
+test('index > plugin works', async function () {
+  const oldFile = [
+    `module('Integration | Component | hello', function (hooks) {`,
+    `  setupRenderingTest(hooks);`,
+    `  `,
+    `  hooks.beforeEach(function (this: TestContext) {`,
+    `    this.name = 'Zoey';`,
+    `  });`,
+    `  `,
+    `  test('it renders', async function (this: TestContext, assert) {`,
+    `    await render<TestContext>(hbs\``,
+    `      <Hello`,
+    `        @name={{this.name}}`,
+    `      />`,
+    `    \`);`,
+    `  });`,
+    `});`,
+  ].join('\n');
+
+  const newFile = await formatFile(oldFile);
+
+  assert.strictEqual(
+    newFile,
+    [
+      `module('Integration | Component | hello', function (hooks) {`,
+      `  setupRenderingTest(hooks);`,
+      ``,
+      `  hooks.beforeEach(function (this: TestContext) {`,
+      `    this.name = 'Zoey';`,
+      `  });`,
+      ``,
+      `  test('it renders', async function (this: TestContext, assert) {`,
+      `    await render<TestContext>(hbs\``,
+      `      <Hello`,
+      `        @name={{this.name}}`,
+      `      />`,
+      `    \`);`,
+      `  });`,
+      `});`,
+      ``,
+    ].join('\n'),
+  );
+});
